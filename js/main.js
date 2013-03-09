b284cb6eb199ad3fc141d7298f3190711c4a2fc7
Ayman Farhat <ayman.farhat1@gmail.com>
2013-03-09 20:16:02 +0200

    Load Github repos

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
 $(document).ready(function(){
-	showGitRepos("aymanfarhat");
+	showGitRepos("aymanfarhat","fork","created","desc");
 });
 
-function showGitRepos(username)
+function showGitRepos(username,type,sort,direction)
 {
 	$.ajax({
 		type:"GET",
-		url:"https://api.github.com/users/"+username+"/repos",
+		url:"https://api.github.com/users/"+username+"/repos?type="+type+"&sort="+sort+"&direction="+direction,
 		dataType:"json",
 		success:function(result)
 		{
-			console.log(result);
+			$.each(result,function(){
+				$('.gitrepos').append("<li><a href='"+this.clone_url+"' target='_null'>"+this.name+"</a>"+ this.description+"</li>");
+			});
 		}
 	});
+}
+
+function showBlogPosts()
+{
+
 }
