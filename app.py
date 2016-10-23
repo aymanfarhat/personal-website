465d6f0ac9a74914eabc40b8e5d4d240ef5cbbaf
Ayman <ayman.farhat1@gmail.com>
2016-10-23 19:08:41 +0300

    Setup basic templates and routes

--- app.py
+++ app.py
@@ -5,9 +5,15 @@ from flask import render_template
 app = Flask(__name__)
 app.debug = True
 
+
 @app.route('/')
-def hello():
-    return render_template('index.html')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
 
 if __name__ == "__main__":
     app.run()
