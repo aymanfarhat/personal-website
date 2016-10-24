f726a836cdc286cebf2543070b16c30f34b03b01
Ayman <ayman.farhat1@gmail.com>
2016-10-24 12:12:55 +0300

    active menu highlighting setup

--- app.py
+++ app.py
@@ -5,15 +5,34 @@ from flask import render_template
 app = Flask(__name__)
 app.debug = True
 
+def get_nav_bar():
+    return [
+        ('/', 'about', 'About'),
+        ('/portfolio', 'portfolio', 'Portfolio'),
+        ('/writing', 'writing', 'Writing'),
+        ('/talks', 'talks', 'Talks'),
+        ('/bookmarks', 'bookmarks', 'Bookmarks')
+    ]
+
 
 @app.route('/')
 def about():
-    return render_template('about.html')
+    data = {
+        'nav_list': get_nav_bar(),
+        'active_page': 'about'
+    }
+
+    return render_template('about.html', view_data = data)
 
 
 @app.route('/portfolio')
 def portfolio():
-    return render_template('portfolio.html')
+    data = {
+        'nav_list': get_nav_bar(),
+        'active_page': 'portfolio'
+    }
+
+    return render_template('portfolio.html', view_data = data)
 
 if __name__ == "__main__":
     app.run()
